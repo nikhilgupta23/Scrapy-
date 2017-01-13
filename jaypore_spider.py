3fdd96fbca7a098fca59a4c169af0f98e04b6b27
nikhilgupta23 <nanonikhil@gmail.com>
2017-01-13 22:56:38 +0530

    Update jaypore_spider to extract all Home Decor products

--- jaypore_spider.py
+++ jaypore_spider.py
@@ -7,6 +7,22 @@ from html.parser import HTMLParser
 def remove_non_ascii(text):
     return ''.join([i if ord(i) < 128 else '' for i in text])
 
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return '|'.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
 class HCVID196_www_jaypore_com_Spider(scrapy.Spider):
     name = "HCVID196.www.jaypore.com"
     products_seen = []
@@ -22,46 +38,62 @@ class HCVID196_www_jaypore_com_Spider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
         
     def parse(self, response):
-        x = response.css('li.nav-items')
+        x = response.css('li.nav-items')        
         for columns in x:
-            y = columns.css('section.topnav-column ul.topnav-menu-links')            
-            for z in y:
-                head_category = z.css('a h3::text').extract_first()
-                list_items = z.css('li')
-                for item in list_items:
-                    page = item.css('a::attr(href)').extract_first()
-                    sub_category = item.css('a::text').extract_first()
-                    if page is not None:
-                        request = scrapy.Request(url=page, callback = self.parsePage)
-                        request.meta['category'] = str(head_category) + '|' + str(sub_category)
-                        yield request
+            if columns.css('a::text').extract_first() == 'HOME & DECOR':
+                y = columns.css('section.topnav-column ul.topnav-menu-links')            
+                for z in y:
+                    head_category = z.css('a h3::text').extract_first()
+                    list_items = z.css('li')
+                    for item in list_items:                        pri
+                        page = item.css('a::attr(href)').extract_first()
+                        sub_category = item.css('a::text').extract_first()
+                        if page is not None:
+                            request = scrapy.Request(url=page, callback = self.parsePage)
+                            request.meta['category'] = str(head_category) + '|' + str(sub_category)
+                            yield request
                         
-    def parsePage(self, response):        
-        products = response.css('li.proditems')        
+    def parsePage(self, response):
+        page = response.request.url.split('?')[0]
+        idnum = re.match('.*?([0-9]+)$', page).group(1)
+        product_link = 'https://www.jaypore.com/shop_sorting_ajax_test.php?shopId=' + idnum + '&sort=featured&p=1&mobile=false&brand_ids=0&sale_only=0'
+        request = scrapy.Request(url=product_link, callback=self.parseOthers)
+        request.meta['category'] = response.meta['category']
+        request.meta['pagenum'] = 1
+        request.meta['idnum'] = idnum
+        yield request
+
+    def parseOthers(self, response):
+        products = json.loads(response.body_as_unicode())
+        if len(products) == 0:
+            return        
         for product in products:            
-            product_link = product.css('a::attr(href)').extract_first()
+            product_link = product["url"]
             if product_link not in HCVID196_www_jaypore_com_Spider.products_seen:
                 HCVID196_www_jaypore_com_Spider.products_seen.append([product_link])
                 request = scrapy.Request(url=product_link, callback=self.parseProduct)
                 request.meta['product_link'] = product_link
                 request.meta['category'] = response.meta['category']                
                 yield request
+        nextpage = response.meta['pagenum'] + 1
+        idnum = response.meta['idnum']
+        product_link = 'https://www.jaypore.com/shop_sorting_ajax_test.php?shopId=' + idnum + '&sort=featured&p=' + str(nextpage) + '&mobile=false&brand_ids=0&sale_only=0'
+        request = scrapy.Request(url=product_link, callback=self.parseOthers)
+        request.meta['category'] = response.meta['category']
+        request.meta['pagenum'] = nextpage
+        request.meta['idnum'] = idnum
+        yield request
                 
     def parseProduct(self, response):
         productLink = response.meta['product_link']
         productCategory = response.meta['category']
         productName = response.css('h1.productName::text').extract_first()
         productImage = response.css('div.lftPanel img::attr(src)').extract_first()
-        productDesc = ''
-        productDescUl = response.css('ul.prdDisc span#prodDesc li')
-        for li in productDescUl:
-            productDesc += li.css('::text').extract_first() + '|'
-        productDesc = productDesc[:-1]
+        productDesc = response.css('ul.prdDisc span#prodDesc').extract_first()
         if not productDesc:
-            productDescUl = response.css('ul.prdDisc span#prodDesc::text')
-            for x in productDescUl:
-                productDesc += x + '|'
-            productDesc = productDesc[:-1]
+            productDesc = 'NA'
+        else:
+            productDesc = strip_tags(productDesc)
         productPrice = response.css('span#dPrice::text').extract_first()
         productSku = response.xpath('//dd[last()]/text()').extract_first()
 
